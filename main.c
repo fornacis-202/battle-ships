b750df181b8f855857375ea7d174e5066abe9ec1
[fornacis-202] <[fornacis.wro@gmail.com]>
2021-02-06 19:37:35 +0330

    added putting ships manually

--- main.c
+++ main.c
@@ -2,6 +2,8 @@
 #include <windows.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <ctype.h>
+#include <math.h>
 
 
 //defenition:
@@ -13,6 +15,8 @@
 #define W 191
 #define E 231
 #define C 79
+#define S 140
+#define Q 135
 #define MAP 112
 
 //global variables:
@@ -20,6 +24,9 @@
 int theme = DEFAULT;
 struct user * list;
 int list_length ;
+int ships_length[10]={5,3,3,2,2,2,1,1,1,1};
+struct node * h[2];
+
 
 
 //structs
@@ -37,6 +44,10 @@ struct ship{
     struct position p2;
     int length;
 };
+struct maps{
+    int main[10][10];
+    int show[10][10];
+}map[2];
 
 //function declaration:
 
@@ -45,6 +56,7 @@ void print_theme();
 void print_menu();
 void print_menu_topic();
 void cls();
+int get_num();
 void change_theme(int * theme);
 int make_user(int player_num);
 int get_user();
@@ -52,6 +64,10 @@ int chose_user(int player_num);
 int score_compare(const void * a1,const void * a2);
 void print_users();
 void save_users();
+void put_ships_manual(int player_num);
+void print_map(int map[10][10]);
+int check_ship_placement(struct ship sh1, int player_num);
+void place_ship_in_main_map(struct ship sh1, int player_num);
 
 //main:
 
@@ -63,7 +79,7 @@ int main() {
     main_menu:
     while (1) {
         print_menu();
-        scanf("%d", &choice);
+        choice = get_num();
         switch (choice) {
             case 1:
 
@@ -74,7 +90,7 @@ int main() {
                     cls();
                     print_menu_topic();
                     printf("\nfirst player:\n\tchose user:\n\t\t1)chose from available users\n\t\t2)new use\n");
-                    scanf("%d", &choice);
+                    choice = get_num();
                     if(!(choice==1 || choice==2)){
                         printf("please enter a valid choice\n");
                         Sleep(1300);
@@ -106,7 +122,7 @@ int main() {
                     cls();
                     print_menu_topic();
                     printf("\nfirst player:\n\tput ships:\n\t\t1)manual\n\t\t2)auto\n");
-                    scanf("%d", &choice);
+                    choice = get_num();
                     if(!(choice==1 || choice==2)){
                         printf("please enter a valid choice\n");
                         Sleep(1300);
@@ -115,6 +131,7 @@ int main() {
                 switch (choice) {
                     case 1:
                         //manual
+                        put_ships_manual(0);
 
                         break;
                     case 2:
@@ -130,7 +147,7 @@ int main() {
                     cls();
                     print_menu_topic();
                     printf("\nsecond player:\n\tchose user:\n\t\t1)chose from available users\n\t\t2)new use\n");
-                    scanf("%d", &choice);
+                    choice = get_num();
                     if(!(choice==1 || choice==2)){
                         printf("please enter a valid choice\n");
                         Sleep(1300);
@@ -160,7 +177,7 @@ int main() {
                     cls();
                     print_menu_topic();
                     printf("\nsecond player:\n\tput ships:\n\t\t1)manual\n\t\t2)auto\n");
-                    scanf("%d", &choice);
+                    choice = get_num();
                     if(!(choice==1 || choice==2)){
                         printf("please enter a valid choice\n");
                         Sleep(1300);
@@ -169,6 +186,7 @@ int main() {
                 switch (choice) {
                     case 1:
                         //manual
+                        put_ships_manual(1);
 
                         break;
                     case 2:
@@ -191,7 +209,7 @@ int main() {
                     cls();
                     print_menu_topic();
                     printf("\nhuman player:\n\tchose user:\n\t\t1)chose from available users\n\t\t2)new use\n");
-                    scanf("%d", &choice);
+                    choice = get_num();
                     if(!(choice==1 || choice==2)){
                         printf("please enter a valid choice\n");
                         Sleep(1300);
@@ -222,7 +240,7 @@ int main() {
                     cls();
                     print_menu_topic();
                     printf("\nhuman player:\n\tput ships:\n\t\t1)manual\n\t\t2)auto\n");
-                    scanf("%d", &choice);
+                    choice = get_num();
                     if(!(choice==1 || choice==2)){
                         printf("please enter a valid choice\n");
                         Sleep(1300);
@@ -231,6 +249,7 @@ int main() {
                 switch (choice) {
                     case 1:
                         //manual
+                        put_ships_manual(0);
 
                         break;
                     case 2:
@@ -256,7 +275,7 @@ int main() {
                     cls();
                     print_menu_topic();
                     printf("\nsetting:\n\t1)theme\n");
-                    scanf("%d", &choice);
+                    choice = get_num();
                     if(!(choice==1)){
                         printf("please enter a valid choice\n");
                         Sleep(1300);
@@ -280,11 +299,16 @@ int main() {
 
                     printf("\n\tno user yet!\n");
                     Sleep(1300);
-                    goto main_menu;
-                }
-                print_users();
 
-                qsort(list,list_length,sizeof(struct user),score_compare);
+                }else {
+                    fflush(stdin);
+                    qsort(list, list_length, sizeof(struct user), score_compare);
+                    print_users();
+                    Sleep(1300);
+                    printf("\npress enter to back to the main menu");
+                    getchar();
+                }
+
 
 
                 break;
@@ -491,7 +515,7 @@ void insert_at_end(struct ship sh){
 
 }
 
-void delete(long int id){
+void delete(){
 
 
      if(current==head){
@@ -503,4 +527,230 @@ void delete(long int id){
         free(current);
 
     }
+}
+void put_ships_manual(int player_num){
+    int i=0;
+    struct ship sh1;
+    while(i<10) {
+        cls();
+        print_menu_topic();
+        printf("\n %s put ships :  \n", player[player_num].name);
+        print_map(map[player_num].main);
+        change_txt_color(theme);
+        printf("\nput the %d space length ship :\nenter first point's i (row)  and j (column) :\n",ships_length[i]);
+        sh1.length=ships_length[i];
+        sh1.p1.i=get_num();
+        sh1.p1.j=get_num();
+        printf("enter the second point's i (row) and j (column) : \n");
+        sh1.p2.i=get_num();
+        sh1.p2.j=get_num();
+        if(check_ship_placement(sh1,player_num)){
+            place_ship_in_main_map(sh1,player_num);
+            if(i==0){
+                insert_first(sh1);
+                h[player_num]=head;
+            } else{
+                head=h[player_num];
+                insert_at_end(sh1);
+            }
+            i++;
+
+        }else{
+            printf("invalid input parameters\n");
+            Sleep(1300);
+        }
+
+
+
+    }
+
+
+
+
+}
+void print_map(int map[10][10]){
+
+    change_txt_color(MAP);
+    printf("  ");
+    for(int j=0;j<10;j++)
+        printf("  %d    ",j);
+    change_txt_color(theme);
+    printf("\n");
+
+    for(int i=0;i<10;i++){
+        //first row
+
+        change_txt_color(MAP);
+        printf("  ");
+
+        for(int j=0;j<10;j++){
+            if(map[i][j]==0){
+                change_txt_color(W);
+                printf("     ");
+            }else if(map[i][j]==1){
+                change_txt_color(E);
+                printf("     ");
+            }else if(map[i][j]==2){
+                change_txt_color(C);
+                printf("     ");
+            }else if(map[i][j]==3){
+                change_txt_color(S);
+                printf("     ");
+            }else if(map[i][j]==-1){
+            change_txt_color(Q);
+            printf("     ");
+            }
+            change_txt_color(MAP);
+            printf("  ");
+
+        }
+        //second row
+        change_txt_color(theme);
+        printf("\n");
+        change_txt_color(MAP);
+        printf(" %d",i);
+
+        for(int j=0;j<10;j++){
+            if(map[i][j]==0){
+                change_txt_color(W);
+                printf("  W  ");
+            }else if(map[i][j]==1){
+                change_txt_color(E);
+                printf("  E  ");
+            }else if(map[i][j]==2){
+                change_txt_color(C);
+                printf("  C  ");
+            }else if(map[i][j]==3){
+            change_txt_color(S);
+            printf("  *  ");
+        }else if(map[i][j]==-1){
+            change_txt_color(Q);
+            printf("  ?  ");
+        }
+            change_txt_color(MAP);
+            printf("  ");
+
+        }
+        //third row
+        change_txt_color(theme);
+        printf("\n");
+        change_txt_color(MAP);
+        printf("  ");
+
+        for(int j=0;j<10;j++){
+            if(map[i][j]==0){
+                change_txt_color(W);
+                printf("     ");
+            }else if(map[i][j]==1){
+                change_txt_color(E);
+                printf("     ");
+            }else if(map[i][j]==2){
+                change_txt_color(C);
+                printf("     ");
+            }else if(map[i][j]==3){
+                change_txt_color(S);
+                printf("     ");
+            }else if(map[i][j]==-1){
+                change_txt_color(Q);
+                printf("     ");
+            }
+            change_txt_color(MAP);
+            printf("  ");
+
+        }
+        //border
+        change_txt_color(theme);
+        printf(" \n");
+
+        change_txt_color(MAP);
+        printf("  ");
+        for(int j=0;j<10;j++)
+            printf("       ");
+        change_txt_color(theme);
+        printf(" \n");
+
+    }
+}
+
+int get_num(){
+    char buff[10];
+    int bool=1;
+
+    scanf("%s",buff);
+    for(int i=0;buff[i]!='\0';i++){
+        if(isdigit(buff[i])==0)
+                bool=0;
+
+    }
+    if(bool==0){
+        return -1;
+
+    }else {
+        int choice ;
+        sscanf(buff,"%d",&choice);
+        return choice;
+    }
+
+
+}
+int check_ship_placement(struct ship sh1, int player_num){
+    if((sh1.p1.i > 9 || sh1.p1.i < 0) || (sh1.p1.j > 9 || sh1.p1.j < 0) || (sh1.p2.i > 9 || sh1.p2.i < 0) ||(sh1.p2.j > 9 || sh1.p2.j < 0))
+        return 0;
+    if(sh1.p1.i==sh1.p2.i && abs(sh1.p1.j-sh1.p2.j)==sh1.length-1){
+        int t = (sh1.p2.j-sh1.p1.j >=0) ? 1 : -1;
+        sh1.p1.j-=t;
+        while (sh1.p2.j!=sh1.p1.j-2*t){
+            if(map[player_num].main[sh1.p1.i][sh1.p1.j]!=0)
+                return 0;
+            if(sh1.p1.i + 1 <=9)
+                if(map[player_num].main[sh1.p1.i+1][sh1.p1.j]!=0)
+                    return 0;
+            if(sh1.p1.i - 1 >=0)
+                if(map[player_num].main[sh1.p1.i-1][sh1.p1.j]!=0)
+                    return 0;
+
+            sh1.p1.j +=t;
+        }
+        return 1;
+
+
+    }else if(sh1.p1.j==sh1.p2.j && abs(sh1.p1.i-sh1.p2.i)==sh1.length-1){
+        int t = (sh1.p2.i-sh1.p1.i >=0)? 1 : -1 ;
+        sh1.p1.i -=t;
+        while (sh1.p2.i!=sh1.p1.i-2*t){
+            if(map[player_num].main[sh1.p1.i][sh1.p1.j]!=0)
+                return 0;
+            if(sh1.p1.j + 1 <=9)
+                if(map[player_num].main[sh1.p1.i][sh1.p1.j+1]!=0)
+                    return 0;
+            if(sh1.p1.j - 1 >=0)
+                if(map[player_num].main[sh1.p1.i][sh1.p1.j-1]!=0)
+                    return 0;
+
+            sh1.p1.i +=t;
+        }
+        return 1;
+
+
+
+    }else
+        return 0;
+}
+void place_ship_in_main_map(struct ship sh1, int player_num){
+
+    if(sh1.p1.i==sh1.p2.i){
+        int t = (sh1.p2.j-sh1.p1.j >=0) ? 1 : -1;
+        while (sh1.p2.j!=sh1.p1.j-t){
+            map[player_num].main[sh1.p1.i][sh1.p1.j]=3;
+            sh1.p1.j += t;
+        }
+    }else if(sh1.p1.j==sh1.p2.j){
+        int t = (sh1.p2.i-sh1.p1.i >=0)? 1 : -1 ;
+        while (sh1.p2.i!=sh1.p1.i-t){
+            map[player_num].main[sh1.p1.i][sh1.p1.j]=3;
+            sh1.p1.i += t;
+        }
+    }
+
+
 }
